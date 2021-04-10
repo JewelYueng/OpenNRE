5dafca8866b55053dcd3caffac6b19b3b89d2b86
JewelYueng <414608192@qq.com>
2021-04-10 09:38:18 +0800

    update train.sh

--- train.sh
+++ train.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 echo "start to train, the scipt is ${$0}"
 # 训练PCNN+ATT+NYT-CEDA
-CUDA_VISIBLE_DEVICES=2 python example/train_bag_pcnn_att.py --train_file=benchmark/nyt10-aug/nyt10_train.txt --val_file=benchmark/nyt10-aug/nyt10_val.txt --test_file=benchmark/nyt10-aug/nyt10_test.txt --rel2id_file=benchmark/nyt10-aug/nyt10_rel2id.json --ckpt=pcnn_with_nyt_augmented
+CUDA_VISIBLE_DEVICES=2 python example/train_bag_pcnn_att.py --train_file=benchmark/nyt10-aug/nyt_train_aug8.txt --val_file=benchmark/nyt10-aug/nyt10_val.txt --test_file=benchmark/nyt10-aug/nyt10_test.txt --rel2id_file=benchmark/nyt10-aug/nyt10_rel2id.json --ckpt=pcnn_with_nyt_augmented
 
 # 训练PCNN+ATT+NYT
-CUDA_VISIBLE_DEVICES=2 python example/train_bag_pcnn_att.py --train_file=benchmark/nyt10/nyt10_train.txt --val_file=benchmark/nyt10/nyt10_val.txt --test_file=benchmark/nyt10/nyt10_test.txt --rel2id_file=benchmark/nyt10-aug/nyt10_rel2id.json 
+CUDA_VISIBLE_DEVICES=0 python example/train_bag_pcnn_att.py --train_file=benchmark/nyt10/nyt10_train.txt --val_file=benchmark/nyt10/nyt10_val.txt --test_file=benchmark/nyt10/nyt10_lt_test.txt --rel2id_file=benchmark/nyt10-aug/nyt10_rel2id.json --ckpt=pcnn_with_nyt
 
 # 训练CNN+ATT+NYT-CEDA
 CUDA_VISIBLE_DEVICES=0 python example/train_bag_cnn_att.py --train_file=benchmark/nyt10-aug/nyt10_train.txt --val_file=benchmark/nyt10-aug/nyt10_val.txt --test_file=benchmark/nyt10-aug/nyt10_test.txt --rel2id_file=benchmark/nyt10-aug/nyt10_rel2id.json --ckpt=cnn_with_nyt_augmented
